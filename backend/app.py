2c6ac503301446d347d3e551a35a0ca42a7695f9
xisigui <nagato.leet@gmail.com>
2025-03-14 00:13:58 +0800

    Added structured response from ai

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,21 @@
 from openai import OpenAI
+from pydantic import BaseModel
 
-client = OpenAI(
-  api_key=""
-)
+client = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
 
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
+class ResponseModel(BaseModel):
+    title: str
+    description: str
+    goals: list[str]
+
+completion = client.beta.chat.completions.parse(
+  model="llama3.1:8b",
   messages=[
-    {"role": "user", "content": "what color is the sky?"},
-  ]
+    {"role": "system", "content": "You are a google scholar search engine. You help users generate capstone ideas."},
+    {"role": "user", "content": "generate capstone ideas with title and a short description about the idea"},
+  ],
+  response_format=ResponseModel
 )
 
-print(completion.choices[0].message)
+generated_result = completion.choices[0].message.parsed
+print(generated_result)
